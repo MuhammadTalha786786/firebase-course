b3e15c2e68bc016b6cb6c72044e90b9f343ceaf5
MuhammadTalha786786 <mtalha25800@gmail.com>
2022-09-27 21:23:09 +0500

    google sign in authentication , phone number authentication

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,33 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react';
 
-const Home = () => {
+const Home = ({route}) => {
+  const {email, pic} = route.params;
+  console.warn(pic);
   return (
     <View>
-      <Text style={{color: 'red', fontSize: 30}}>Home</Text>
+      <Text style={{color: 'red', fontSize: 30}}>{email}</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: pic,
+        }}
+      />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
 export default Home;
